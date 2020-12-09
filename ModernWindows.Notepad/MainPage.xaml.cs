7ddd47a70e32a6fda685b8fc327c56d97f406882
Narek Torosyan <narek.email@gmail.com>
2020-12-09 20:22:02 +0400

    Add open file
    So far it only works with Unicode

--- ModernWindows.Notepad/MainPage.xaml.cs
+++ ModernWindows.Notepad/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -15,20 +16,42 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace ModernWindows.Notepad
-{
+namespace ModernWindows.Notepad {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
             this.InitializeComponent();
         }
 
-        private void AppBarToggleButton_Click(object sender, RoutedEventArgs e) {
+        private async void CommandBarButton(object sender, RoutedEventArgs e) {
+            var button = sender as AppBarButton;
+            if (button.Name == "NewFile") {
+                
+            } else if (button.Name == "OpenFile") {
+                // Open a text file.
+                Windows.Storage.Pickers.FileOpenPicker open =
+                    new Windows.Storage.Pickers.FileOpenPicker();
+                open.SuggestedStartLocation =
+                    Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+                open.FileTypeFilter.Add(".rtf");
+                open.FileTypeFilter.Add(".txt");
 
+                Windows.Storage.StorageFile file = await open.PickSingleFileAsync();
+
+                if (file != null) {
+                    using (Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                        await file.OpenAsync(Windows.Storage.FileAccessMode.Read)) {
+                        // Load the file into the Document property of the RichEditBox.
+                        var format = Windows.UI.Text.TextSetOptions.FormatRtf;
+                        if (file.Name.EndsWith(".txt"))
+                            format = Windows.UI.Text.TextSetOptions.None;
+                        EditPane.Document.LoadFromStream(format, randAccStream);
+                    }
+                }
+
+            }
         }
     }
 }
